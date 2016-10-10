2e6a51dce8b6159a4b93520a4d51e70cc8aa1452
lucky <fanqietou@126.com>
2016-10-10 21:28:32 +0800

    2016/10/10 modify by lucky

--- java/sy/util/base/MD5Util.java
+++ java/sy/util/base/MD5Util.java
@@ -11,11 +11,6 @@ import java.security.NoSuchAlgorithmException;
  */
 public class MD5Util {
 
-	public static void main(String[] args) {
-		String s = "孙宇";
-		System.out.println(md5(s));
-	}
-
 	/**
 	 * md5加密
 	 * 
